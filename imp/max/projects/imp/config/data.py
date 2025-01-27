ed144fd8ce8f15c5184a7c0cd053146f544d0c1c
jingyang-kylix <jingyang@sciforium.com>
2025-01-27 21:41:43 +0000

    fix BATCH_SIZE assignment

--- imp/max/projects/imp/config/data.py
+++ imp/max/projects/imp/config/data.py
@@ -55,7 +55,7 @@ num_vision_classes = datasets_config.num_vision_classes
 num_audio_classes = datasets_config.num_audio_classes
 
 
-BASE_TRAIN_BATCH_SIZE = os.environ.get('BATCH_SIZE', 768)
+BASE_TRAIN_BATCH_SIZE = int(os.environ.get('BATCH_SIZE', 768))
 print(f'****** BATCH_SIZE: {BASE_TRAIN_BATCH_SIZE} ******')
 BASE_VIDEO_TRAIN_BATCH_SIZE = BASE_TRAIN_BATCH_SIZE // 4
 BASE_EVAL_BATCH_SIZE = 1024
